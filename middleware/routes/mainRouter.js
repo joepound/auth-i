238a8aad7fb80693636e6ce4cb1ce0235b4117b0
joepound <14357797+joepound@users.noreply.github.com>
2019-02-25 19:13:18 -0700

    Added endpoint to GET all users (with token-based authentication)

--- middleware/routes/mainRouter.js
+++ middleware/routes/mainRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const db = require("../../data/dbConfig");
 const sendError = require("../errors/errorHandler");
 
@@ -23,13 +24,16 @@ router.post("/register", (req, res) => {
     console.log("Proceeding to register the new user...");
     db("Users")
       .insert(userData)
-      .then(() =>
+      .then(() => {
         res
           .status(201)
-          .json({ UserName: userData.UserName, UserPassword: hash })
-      )
-      .catch(err => sendError(res, 500, err.errno || err.toString()))
-      .finally(console.log("User registration attempt finished."));
+          .json({ UserName: userData.UserName, UserPassword: hash });
+        console.log("User registration attempt finished.");
+      })
+      .catch(err => {
+        sendError(res, 500, err.errno || err.toString());
+        console.log("User registration attempt finished.");
+      });
   }
 });
 
@@ -57,22 +61,55 @@ router.post("/login", (req, res) => {
           if (
             bcrypt.compareSync(userData.UserPassword, userMatch.UserPassword)
           ) {
-            res.status(200).json("One has simply logged in.");
+            const token = jwt.sign(userMatch, process.env.SECRET_KEY, {
+              expiresIn: 60
+            });
+            res.status(200).json({ success: true, token });
           } else {
             sendError(res, 401, "You shall not pass!");
           }
         } else {
           sendError(res, 404, "You shall not pass!");
         }
-        console.log("User login attempt finished.")
+        console.log("User login attempt finished.");
       })
       .catch(err => {
-        console.log("User login attempt finished.")
         sendError(res, 500, err.errno || err.toString());
+        console.log("User login attempt finished.");
       });
   }
 });
 
-router.get("/users", (req, res) => {});
+router.get(
+  "/users",
+  (req, res, next) => {
+    const token = req.get("Authorization");
+    if (token) {
+      console.log(token)
+      jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+          sendError(res, 401, "You shall not pass!");
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
+    } else {
+      sendError(res, 401, "You shall not pass!");
+    }
+  },
+  (req, res) => {
+    console.log("Attempting to GET all users...");
+    db("Users")
+      .then(users => {
+        res.status(200).json({ success: true, users });
+        console.log("GET attempt for all users finished.");
+      })
+      .catch(err => {
+        sendError(res, 500, err.errno || err.toString());
+        console.log("GET attempt for all users finished.");
+      });
+  }
+);
 
 module.exports = router;
